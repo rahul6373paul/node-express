a70f523216ad175dc71d6737781fed512d472d17
rahul6373paul <rahul6373paul@github.com>
2020-09-09 20:30:48 +0530

    express router routes added

--- index.js
+++ index.js
@@ -5,34 +5,14 @@ const bodyParser = require('body-parser')
 
 const hostname = 'localhost';
 const port = 3000;
+const dishRouter = require('./routes/dishRouter')
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/dishes',(req, res, next)=>{
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-})
-
-app.get('/dishes',(req, res, next)=>{
-    res.end('Will send all the dishes to you!');
-})
-
-app.post('/dishes', (req, res,next) =>{
-    res.end('will add the dish: '+ req.body.name + 'with details: ' + req.body.description );
-})
-
-app.put('/dishes', (req, res,next) =>{
-    res.statusCode = 403;
-    res.end('PUT operation not supported on dishes');
-})
-
-app.delete('/dishes', (req, res,next) =>{
-    res.end('Deleting all the dishes');
-})
+app.use('/dishes', dishRouter)
 
 app.get('/dishes/:dishId',(req, res, next)=>{
     res.end('Will send details of the dish: ' + req.params.dishId + ' to you');
