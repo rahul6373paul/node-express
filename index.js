e06461bfcbfbf4bd5ac7fc422eb94750cb6978f1
rahul6373paul <rahul6373paul@github.com>
2020-09-09 21:22:30 +0530

    added mini routes of promotions and leaders

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 const hostname = 'localhost';
 const port = 3000;
 const dishRouter = require('./routes/dishRouter')
+const promoRouter = require('./routes/promoRouter')
+const leaderRouter = require('./routes/leaderRouter')
 
 const app = express();
 
@@ -13,24 +15,8 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/dishes', dishRouter)
-
-app.get('/dishes/:dishId',(req, res, next)=>{
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you');
-})
-
-app.post('/dishes/:dishId', (req, res,next) =>{
-    res.statusCode = 403;
-    res.end('POST operation not supported on dishes: '+ req.params.dishId);
-})
-
-app.put('/dishes/:dishId', (req, res,next) =>{
-    res.write('Updating the dish: ' + req.params.dishId + ' for you');
-    res.end('Will update the dish: ' + req.params.dishId + ' for you with detaitls '+ req.body.description);
-})
-
-app.delete('/dishes/:dishId', (req, res,next) =>{
-    res.end('Deleting dish: '+ req.params.dishId);
-})
+app.use('/promotions', promoRouter)
+app.use('/leaders', leaderRouter)
 
 app.use(express.static(__dirname+'/public'))
 
